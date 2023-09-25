6c5d07a9a759d5c2b42f6b613955c1df44268eaf
esrojas27 <esrojas27@gmail.com>
2023-09-24 20:21:00 -0500

    CORS

--- front_gui_nodules/src/environments/environment.ts
+++ front_gui_nodules/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  authUrl: 'http://localhost:8081/',
+  authUrl: 'http://ec2-52-15-218-180.us-east-2.compute.amazonaws.com:9000/ms-admon/',
 };
 
 /*
